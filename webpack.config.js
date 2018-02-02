10eaca6af014c9967cc0d11a3d7ff6af62045b29
stepforwardtech <jrstrayhorn@gmail.com>
2018-02-02 13:14:20 -0500

    Fixing typo on home page, adding babel-polyfill, refactoring api.js to ES6

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var webpack = require('webpack');
 // NODE_ENV to production
 // uglify
 var config = {
-  entry: './app/index.js',
+  entry: ['babel-polyfill', './app/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
