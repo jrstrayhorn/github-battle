5d3af8b3c5bed7ddbddc2a888bfc662a96bec694
stepforwardtech <jrstrayhorn@gmail.com>
2018-02-02 14:55:07 -0500

    Fixing bug on popular and api files.. refactoring loading to use ES6

--- app/utils/api.js
+++ app/utils/api.js
@@ -54,9 +54,7 @@ module.exports = {
   },
   fetchPopularRepos(language) {
     const encodedURI = window.encodeURI(
-      `https://api.github.com/search/repositories?q=stars:>1+language:
-        ${language}
-        &sort=stars&order=desc&type=Repositories`
+      `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
     );
     return axios.get(encodedURI).then(({ data }) => data.items);
   }
